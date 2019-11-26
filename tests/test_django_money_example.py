2aebbd27e3c6af2fbd8d9ad65ef32edb61097f70
Johnny Rocketfingers <developer94@gmail.com>
2019-11-26 03:50:45 +0100

    Add a SQL migration test to debug django-money#530

--- tests/test_django_money_example.py
+++ tests/test_django_money_example.py
@@ -1,5 +1,22 @@
-from django_money_example import __version__
+import pytest
+from django.db import connections
+from django.db.migrations.executor import MigrationExecutor
+
+from django_money_example.core import models
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+@pytest.mark.django_db
+def test_sql_migrations():
+    connection = connections["default"]
+    executor = MigrationExecutor(connection)
+
+    migration = executor.loader.get_migration_by_prefix("core", "0002")
+    targets = [("core", migration.name)]
+    plan = [(executor.loader.graph.nodes[targets[0]], False)]
+
+    import pdb; pdb.set_trace()
+    sql_statements = executor.collect_sql(plan)
